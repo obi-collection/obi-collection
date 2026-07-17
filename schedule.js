fea50310ea0e1b1eaa6fa9b7669c3d3286e4ead0
三角昭太 <misumi2026g@gmail.com>
2026-07-17 22:41:36 +0900

    Update scheduled X posts from the schedule editor

--- schedule.js
+++ schedule.js
@@ -8,16 +8,6 @@ const SCHEDULE_DATA = {
       "text": "De La Soulをたっぷり聴きたい時はこのDJミックス。\n公式MVがふんだんに使われていて、耳だけでなく目でも楽しめる内容です。\n手元アップでスキルフルなスクラッチや2枚使いが見れて勉強になるのですが、一番の見どころは50分間一切ダレないグルーヴキープ力です。",
       "youtube": "https://youtu.be/WsyGcx2iWv4",
       "enabled": true
-    },
-    {
-      "id": "spreviewtest",
-      "month": 10,
-      "day": 10,
-      "title": "レビュー後テスト",
-      "text": "pathspecコミットのテスト。",
-      "youtube": "https://youtu.be/dQw4w9WgXcQ",
-      "enabled": true,
-      "debut": "none"
     }
   ]
 };
